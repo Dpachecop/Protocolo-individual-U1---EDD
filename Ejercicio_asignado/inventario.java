3a991f718e099c8cd07e7fe7c912153f9da572d0
Daniel pacheco <dpachper@gmail.com>
2025-09-04 08:22:22 -0500

    Realizacion de el ejercicio asignado - #17

--- Ejercicio_asignado/inventario.java
+++ Ejercicio_asignado/inventario.java
@@ -3,6 +3,14 @@ package Ejercicio_asignado;
 public class inventario {
     public static void main(String[] args) {
         
+    String[][] inAndOut = {
+      {"Entrada: 23:45", "08:40"},
+      {"Salida: 23:40", "5:40"},
+      {"Entrada: 23:40", "5:40"}
+    };
+    Producto producto = new Producto("Champu", "", inAndOut);
+    producto.mostrarRegistros(0);
+
     }
 }
 
--- Ejercicio_asignado/producto.java
+++ Ejercicio_asignado/producto.java
@@ -1,5 +1,19 @@
 package Ejercicio_asignado;
 
-public class producto {
-    public String nombre;
+public record Producto(String nombre,
+        String descripcion,
+        String[][] inAndOut) {
+
+    public void mostrarRegistros(int numeroIngreso) {
+        System.out.print("Regstro de entradas y salidas" + "\n");
+        for (int i = 0; i < inAndOut.length; i++) {
+            for (int j = 0; j < inAndOut[i].length; j++) {
+                System.out.print(inAndOut[i][numeroIngreso] + "\t");
+             break;
+            }
+            System.out.println();
+          
+        }
+
+    }
 }
